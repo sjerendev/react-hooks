449c5f86b349b7b4146ae7f00351d7c8bf1e88a1
Patrik Sjeren <groovepromo@gmail.com>
2023-09-13 21:51:33 +0200

    230913

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+    const sound = useRef();
+    const color = useRef();
+
+    const submit = (e) => {
+        e.preventDefault();
+        const soundVal = sound.current.value;
+        const colorVal = color.current.value;
+        alert(`${soundVal} sounds like ${colorVal}`);
+        sound.current.value = '';
+        color.current.value = '#000000';
+    };
+    return (
+        <form onSubmit={submit}>
+            <input ref={sound} type="text" placeholder="Sound..." />
+            <input ref={color} type="color" />
+            <button>ADD</button>
+        </form>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +30,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
